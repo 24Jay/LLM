6c304dfbc202ce74182e0c417ddf3024cbd8ae36
quantux <quantux@quantuxdeMacBook-Pro.local>
2025-03-16 10:57:30 +0800

    transformer

--- transformer/mini_transformer.py
+++ transformer/mini_transformer.py
@@ -181,5 +181,26 @@ class Encoder(torch.nn.Module):
 
 
 class DecoderBlock(torch.nn.Module):
-    def __init__(self, ) -> None:
+    def __init__(self, self_attention: MultiHeadAttention, cross_attention: MultiHeadAttention, feedforward: FeedForwardBlock, dropout:float) -> None:
+        super().__init__()
+
+        self.self_attention = self_attention
+        self.cross_attention = cross_attention
+        self.feedforward = feedforward
+
+        self.dropout = torch.nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+        self.residual_connection = torch.nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        x = self.residual_connection[0](x, lambda x: self.self_attention(x, x, x, tgt_mask))
+        x = self.residual_connection[1](x, lambda x: self.cross_attention(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connection[2](x, lambda x: self.feedforward(x))
+        return x
+
+
+class ProjectionLayer(torch.nn.Module):
+
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
